ec9f0f9af3b184ac7809b2952d3983320e2628f8
musamthembu84 <musamthembu84@gmail.com>
2026-09-11 22:19:18 +0200

    Fix Redis connection temp

--- core/redis_client.py
+++ core/redis_client.py
@@ -1,10 +1,7 @@
 import redis
 
-from core.config import settings
-
-
 redis_client = redis.Redis(
-    host="dokotela-redis.7ctctb.0001.use1.cache.amazonaws.com",
+    host="10.0.3.206",
     port=6379,
     db=0,
 )
