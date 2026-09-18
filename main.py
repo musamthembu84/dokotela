7e133df57c5add4a6ac08cc24ed39361f817c4d5
musamthembu84 <musamthembu84@gmail.com>
2026-09-18 23:59:51 +0200

    update cors

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://23.20.254.20:3000",
+        "http://98.95.246.110:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
